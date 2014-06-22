ac8f29628e030a6d324408fd41a6a24093ce4e8b
Joe Zhou <zhoushuqun@gmail.com>
2014-06-22 13:25:57 +0800

    Fix timeout parameters.

--- src/main/java/com/redv/chbtc/service/polling/CHBTCBaseTradePollingService.java
+++ src/main/java/com/redv/chbtc/service/polling/CHBTCBaseTradePollingService.java
@@ -1,7 +1,5 @@
 package com.redv.chbtc.service.polling;
 
-import org.apache.commons.lang3.math.NumberUtils;
-
 import com.redv.chbtc.CHBTCClient;
 import com.redv.chbtc.CHBTCExchange;
 import com.xeiam.xchange.ExchangeSpecification;
@@ -21,23 +19,21 @@ public class CHBTCBaseTradePollingService extends CHBTCBasePollingService {
 		final String accessKey = exchangeSpecification.getApiKey();
 		final String secretKey = exchangeSpecification.getSecretKey();
 
-		final int socketTimeout = NumberUtils
-				.toInt((String) exchangeSpecification
-						.getParameter(CHBTCExchange.SOCKET_TIMEOUT_PARAMETER));
-		final int connectTimeout = NumberUtils
-				.toInt((String) exchangeSpecification
-						.getParameter(CHBTCExchange.CONNECT_TIMEOUT_PARAMETER));
-		final int connectionRequestTimeout = NumberUtils
-				.toInt((String) exchangeSpecification
-						.getParameter(CHBTCExchange.CONNECTION_REQUEST_TIMEOUT_PARAMETER));
+		final Integer socketTimeout = (Integer) exchangeSpecification
+				.getParameter(CHBTCExchange.SOCKET_TIMEOUT_PARAMETER);
+		final Integer connectTimeout = (Integer) exchangeSpecification
+				.getParameter(CHBTCExchange.CONNECT_TIMEOUT_PARAMETER);
+		final Integer connectionRequestTimeout = (Integer) exchangeSpecification
+				.getParameter(CHBTCExchange.CONNECTION_REQUEST_TIMEOUT_PARAMETER);
 
 		client = new CHBTCClient(
 				tradeApiUrl,
 				accessKey,
 				secretKey,
-				socketTimeout,
-				connectTimeout,
-				connectionRequestTimeout);
+				socketTimeout == null ? 0 : socketTimeout.intValue(),
+				connectTimeout == null ? 0 : connectTimeout.intValue(),
+				connectionRequestTimeout == null ? 0
+						: connectionRequestTimeout.intValue());
 	}
 
 	/**
